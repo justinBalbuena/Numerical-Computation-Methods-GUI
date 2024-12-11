b99db398b7ade79aefe9ee5b44745c1002637582
kcamaj <kcamaj01@manhattan.edu>
2024-12-11 18:16:49 -0500

    removed true error calculations from jacobi method

--- global_functions_and_more/error_option.py
+++ global_functions_and_more/error_option.py
@@ -26,16 +26,10 @@ def approx_mean_error_method():
     return "a"
 def approx_root_error_method():
     return "b"
-def true_mean_error_method():
-    return "c"
-def true_root_error_method():
-    return "d"
 
 error_tolerance_methods_iter = {
     "Approximate mean absolute error (MAE)": approx_mean_error_method,
     "Approximate root mean square error (RMSE)": approx_root_error_method,
-    "True mean absolute error (MAE)": true_mean_error_method,
-    "True root mean square error (RMSE)": true_root_error_method
 }
 
 
